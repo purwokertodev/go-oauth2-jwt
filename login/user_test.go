f8503ef4201f9329e23c686ac495fd7719fb1e6b
Wuriyanto <wuriyanto48@yahoo.co.id>
2017-06-17 05:23:16 +0700

    add travis

---
+++ login/user_test.go
@@ -0,0 +1,19 @@
+package login
+
+import(
+  "testing"
+)
+
+func TestSuccessLogin(t *testing.T) {
+  u := &UserLogin{Username: "Wuriyanto", Password: "123456"}
+  if !u.IsValidUser() {
+    t.Error("Login is invalid")
+  }
+}
+
+func TestFailLogin(t *testing.T) {
+  u := &UserLogin{Username: "Wuriyanto", Password: "12345"}
+  if u.IsValidUser() {
+    t.Error("Login is invalid")
+  }
+}

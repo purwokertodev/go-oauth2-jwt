172d3666901d0f95ac70132d952953147694fd65
Wuriyanto <wuriyanto48@yahoo.co.id>
2017-06-17 05:33:35 +0700

    format code

--- login/user_test.go
+++ login/user_test.go
@@ -1,19 +1,19 @@
 package login
 
-import(
-  "testing"
+import (
+	"testing"
 )
 
 func TestSuccessLogin(t *testing.T) {
-  u := &UserLogin{Username: "Wuriyanto", Password: "123456"}
-  if !u.IsValidUser() {
-    t.Error("Login is invalid")
-  }
+	u := &UserLogin{Username: "Wuriyanto", Password: "123456"}
+	if !u.IsValidUser() {
+		t.Error("Login is invalid")
+	}
 }
 
 func TestFailLogin(t *testing.T) {
-  u := &UserLogin{Username: "Wuriyanto", Password: "12345"}
-  if u.IsValidUser() {
-    t.Error("Login is invalid")
-  }
+	u := &UserLogin{Username: "Wuriyanto", Password: "12345"}
+	if u.IsValidUser() {
+		t.Error("Login is invalid")
+	}
 }
